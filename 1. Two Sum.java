47088d41cf8468eb902a8f0cfe1bbe8d4652044a
Sidney Pho <phosidney@gmail.com>
2020-02-28 23:41:26 -0600

    Create 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        //idea is to create a hashmap, and add in elements that has been iterated over with key: num, value: index
+        //constant time lookups O(1)
+        //recalculate the complement every time to see if the complement matches an element that is in hashmap
+        
+        //create hashmap of Integer keys (numbers) and Integer values (index)
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        //iterate through int array nums
+        for (int i = 0; i < nums.length; i++) {
+            //find target complement for each value iterated
+            int complement = target - nums[i];
+            if (hm.containsKey(complement)) {
+                //get method retrieves the value associated with the complement i.e. the index
+                return new int[]{hm.get(complement), i};
+            } else {
+                //if complement is not found, add into hashmap and continue iterating
+                hm.put(nums[i], i);
+            }
+        }
+        //if no values add up to the target is seen in the integer array, throw an exception
+        throw new IllegalArgumentException("No two values equal target!");
+        
+    }
+}
