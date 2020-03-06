ec53a1ee5d414a273c6e90e880c55c2bc0238a39
Sidney Pho <phosidney@gmail.com>
2020-03-05 18:02:16 -0600

    Update 169. Majority Element.java

--- 169. Majority Element.java
+++ 169. Majority Element.java
@@ -1,5 +1,11 @@
 class Solution {
-    public int majorityElement(int[] nums) {
+    
+    //idea is to create a hashmap to put integers that are seen and map their frequencies
+    //we can short circuit the iteration by checking if the frequency of the number ever gets larger than n/2 (for the majority element)
+    //O(n) time complexity because we have to iterate over the array of size n
+    //O(n) space complexity because we have to create a hashmap of n elements
+    
+    public int mjorityElement(int[] nums) {
         Map<Integer, Integer> hm = new HashMap<>();
         int majorityElement = 0;
         for (int i = 0; i < nums.length; i++) {
