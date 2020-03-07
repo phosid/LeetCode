16d85e6fdbf42776c2d7a5a1e1e3f74665aa8890
Sidney Pho <phosidney@gmail.com>
2020-03-06 21:23:16 -0600

    Create 141. Linked List Cycle.java

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+     //Idea is to use Floyd's Cycle Detection Algorithm which runs in O(n) time using O(1) space
+    
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
